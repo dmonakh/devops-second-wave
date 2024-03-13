68a4f355754a25a56268565454e7c51562098a98
dmonakh <d.monakh@it-sprout.org>
2024-03-13 17:33:54 +0200

    init stage

--- terraform/scripts/create-bucket.sh
+++ terraform/scripts/create-bucket.sh
@@ -2,7 +2,7 @@
 
 if [[ $(gsutil ls gs://${BUCKET_NAME} >/dev/null; echo $?) -eq 1 ]]; then
     echo "===[ Bucket doesn't exist. Creating... ]==="
-    gsutil mb -l us-central1 -c Standard -p $GCP_PROJECT_ID_NONPROD1 gs://$BUCKET_NAME
+    gsutil mb -l us-central1 -c Standard gs://$BUCKET_NAME
 else
     echo "===[ Bucket already exists ]==="
 fi
