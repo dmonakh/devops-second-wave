bb8bd5895c8880c51d1b383871d45a6f79afbc25
dmonakh <d.monakh@it-sprout.org>
2024-03-13 18:29:45 +0200

    init test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,51 +1,29 @@
-# variable "def_name" {
-#   default = "${env.DEF_NAME}" 
-# }
+variable "def_name" {
+  default = "${env.DEF_NAME}" 
+}
 
-# variable "project_id" {}
+variable "project_id" {}
 
-# variable "region_prj" {
-#   default = "${env.REGION}"
-# }
+variable "region_prj" {
+  default = "${env.REGION}"
+}
 
-# variable "zone_prj" {
-#   default = "${env.ZONE}"
-# }
+variable "zone_prj" {
+  default = "${env.ZONE}"
+}
 
-# variable "sa_name" {
-#   default = "${env.BUCKET_NAME}"
-# }
+variable "sa_name" {
+  default = "${env.BUCKET_NAME}"
+}
 
-# variable "sa_account" {
-#   default = "${env.SA_ACC}"
-# }
+variable "sa_account" {
+  default = "${env.SA_ACC}"
+}
 
 # variable "domain" {
 #   default = "it-sproutdevteam.fun" # Replace with your domain.
 # }
 
-
-# variables.tf
-
-variable "REGION" {
-  type    = string
-}
-
-variable "GCP_PROJECT_ID_NONPROD1" {
-  type    = string
-}
-
-variable "project_id" {
-  type    = string
-}
-
-variable "region_prj" {
-  type    = string
-}
-
-variable "zone_prj" {
-  type    = string
-}
 
 variable "local_ip_range" {
   default = "10.100.0.0/28"
