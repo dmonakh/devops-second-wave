61a161806f7e4b892092b7cb3dceb242a917c2b4
dmonakh <d.monakh@it-sprout.org>
2024-03-25 15:05:54 +0200

    init

---
+++ non-prod/terraform/scripts/install.sh
@@ -0,0 +1,11 @@
+helm repo add chatwoot https://chatwoot.github.io/charts
+helm install chatwoot chatwoot/chatwoot -f .env_chat.yml
+
+
+git clone https://github.com/novuhq/novu.git
+cd novu/docker/kubernetes/helm/
+mv Chart.lock
+helm dependency build
+
+kubectl edit svc my-novu-web
+helm install my-novu  ./ --set store.encryptionKey=$(openssl rand -base64 32)
--- scripts/create-bucket.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if gsutil ls gs://${BUCKET_NAME} >/dev/null 2>&1; then
-    echo "===[ Bucket already exists ]==="
-else
-    echo "===[ Bucket doesn't exist. Creating... ]==="
-    gsutil mb -l us-central1 -c Standard gs://${BUCKET_NAME}
-fi
