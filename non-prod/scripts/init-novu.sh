4bcff097793a66fa58211bca1190b6d8acca5c29
dmonakh <d.monakh@it-sprout.org>
2024-03-25 18:54:35 +0200

    init

--- non-prod/scripts/init-novu.sh
+++ non-prod/scripts/init-novu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd novu/docker/kubernetes/helm/
-rm Chart.lock
-helm dependency build
-helm install my-novu  ./ --set store.encryptionKey=$(openssl rand -base64 32)
+ls
+# cd novu/docker/kubernetes/helm/
+# rm Chart.lock
+# helm dependency build
+# helm install my-novu  ./ --set store.encryptionKey=$(openssl rand -base64 32)
